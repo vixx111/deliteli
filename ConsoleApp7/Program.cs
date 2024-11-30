98f208e83142db8336cf25bbcff4e135c4b79a6b
Admin <Admin@DESKTOP-UGP0C2G>
2024-11-30 09:55:26 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите натуральное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Простые делители числа {n}:");
+GetPrimeFactors(n);    
+    static void GetPrimeFactors(int n)
+{
+    for (int i = 2; i <= n; i++)
+    {
+        while (n % i == 0)
+        {
+            Console.Write(i + " ");
+            n /= i;
+        }
+    }
+    Console.WriteLine();
+}
